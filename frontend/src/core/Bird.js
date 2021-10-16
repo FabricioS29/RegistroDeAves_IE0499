ece7723af69f7b114c569a65e1fc2d30910d4ef7
FabricioS29 <fabricio.solrojas@gmail.com>
2021-10-15 22:37:47 -0600

    Avance

--- frontend/src/core/Bird.js
+++ frontend/src/core/Bird.js
@@ -1,10 +1,37 @@
 import React, {useState, useEffect} from "react";
+import { read } from "./apiCore";
+import Card from './Card';
+import Navigation from './Navigation';
 
-const Bird = () => {
+const Bird = (props) => {
+    const [bird, setBird] = useState({});
+    const [error, setError] = useState(false);
+
+    const loadSingleBird = birdId => {
+        read(birdId).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setBird(data);
+            }
+        })
+    }
+
+    useEffect(() => {
+        const birdId = props.match.params.birdId
+        loadSingleBird(birdId);
+    }, [props])
 
     return (
         <>
-            <h2>This is the bird page</h2>
+            <Navigation/>
+            <div className='container'>
+                <h2>This is the bird page</h2>
+                {
+                    bird && 
+                    <Card bird={bird}/>
+                }
+            </div>
         </>
     )
 }
